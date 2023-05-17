f1a704a5273f9699857c46f85092494287316ff6
Misc1170 <sloopy1170@gmail.com>
2023-05-17 11:21:07 +0300

    add queue teach base

---
+++ config.inc.php
@@ -0,0 +1,89 @@
+<?php
+/**
+ *  MODX Configuration file
+ */
+$database_type = 'mysql';
+$database_server = 'fluidline.beget.tech';
+$database_user = 'fluidline_641be6';
+$database_password = 'o&nQ&Xlaj9KZaCL7';
+$database_connection_charset = 'utf8mb4';
+$dbase = 'fluidline_641be6';
+$table_prefix = 'modx_';
+$database_dsn = 'mysql:host=localhost;dbname=fluidline_641be6;charset=utf8mb4';
+$config_options = array (
+);
+$driver_options = array (
+);
+
+$lastInstallTime = 1677591665;
+
+$site_id = 'modx63fe0471c6a366.57395337';
+$site_sessionname = 'SN63fe03e56e31d';
+$https_port = '443';
+$uuid = 'eb88f442-03c0-47a3-923a-2849af5072c3';
+
+if (!defined('MODX_CORE_PATH')) {
+    $modx_core_path= '/home/f/fluidline/kurs.fluid-line.ru/public_html/core/';
+    define('MODX_CORE_PATH', $modx_core_path);
+}
+if (!defined('MODX_PROCESSORS_PATH')) {
+    $modx_processors_path= '/home/f/fluidline/kurs.fluid-line.ru/public_html/core/src/Revolution/Processors/';
+    define('MODX_PROCESSORS_PATH', $modx_processors_path);
+}
+if (!defined('MODX_CONNECTORS_PATH')) {
+    $modx_connectors_path= '/home/f/fluidline/kurs.fluid-line.ru/public_html/connectors/';
+    $modx_connectors_url= '/connectors/';
+    define('MODX_CONNECTORS_PATH', $modx_connectors_path);
+    define('MODX_CONNECTORS_URL', $modx_connectors_url);
+}
+if (!defined('MODX_MANAGER_PATH')) {
+    $modx_manager_path= '/home/f/fluidline/kurs.fluid-line.ru/public_html/manager/';
+    $modx_manager_url= '/manager/';
+    define('MODX_MANAGER_PATH', $modx_manager_path);
+    define('MODX_MANAGER_URL', $modx_manager_url);
+}
+if (!defined('MODX_BASE_PATH')) {
+    $modx_base_path= '/home/f/fluidline/kurs.fluid-line.ru/public_html/';
+    $modx_base_url= '/';
+    define('MODX_BASE_PATH', $modx_base_path);
+    define('MODX_BASE_URL', $modx_base_url);
+}
+if(defined('PHP_SAPI') && (PHP_SAPI == "cli" || PHP_SAPI == "embed")) {
+    $isSecureRequest = false;
+} else {
+    $isSecureRequest = ((isset($_SERVER['HTTPS']) && !empty($_SERVER['HTTPS']) && strtolower($_SERVER['HTTPS']) !== 'off') || $_SERVER['SERVER_PORT'] == $https_port);
+}
+if (!defined('MODX_URL_SCHEME')) {
+    $url_scheme=  $isSecureRequest ? 'https://' : 'http://';
+    define('MODX_URL_SCHEME', $url_scheme);
+}
+if (!defined('MODX_HTTP_HOST')) {
+    if(defined('PHP_SAPI') && (PHP_SAPI == "cli" || PHP_SAPI == "embed")) {
+        $http_host='kurs.fluid-line.ru/';
+        define('MODX_HTTP_HOST', $http_host);
+    } else {
+        $http_host= array_key_exists('HTTP_HOST', $_SERVER) ? htmlspecialchars($_SERVER['HTTP_HOST'], ENT_QUOTES) : 'modx';
+        if ($_SERVER['SERVER_PORT'] != 80) {
+            $http_host = str_replace(':' . $_SERVER['SERVER_PORT'], '', $http_host); // remove port from HTTP_HOST
+        }
+        $http_host .= in_array($_SERVER['SERVER_PORT'], [80, 443]) ? '' : ':' . $_SERVER['SERVER_PORT'];
+        define('MODX_HTTP_HOST', $http_host);
+    }
+}
+if (!defined('MODX_SITE_URL')) {
+    $site_url= $url_scheme . $http_host . MODX_BASE_URL;
+    define('MODX_SITE_URL', $site_url);
+}
+if (!defined('MODX_ASSETS_PATH')) {
+    $modx_assets_path= '/home/f/fluidline/kurs.fluid-line.ru/public_html/assets/';
+    $modx_assets_url= '/assets/';
+    define('MODX_ASSETS_PATH', $modx_assets_path);
+    define('MODX_ASSETS_URL', $modx_assets_url);
+}
+if (!defined('MODX_LOG_LEVEL_FATAL')) {
+    define('MODX_LOG_LEVEL_FATAL', 0);
+    define('MODX_LOG_LEVEL_ERROR', 1);
+    define('MODX_LOG_LEVEL_WARN', 2);
+    define('MODX_LOG_LEVEL_INFO', 3);
+    define('MODX_LOG_LEVEL_DEBUG', 4);
+}
