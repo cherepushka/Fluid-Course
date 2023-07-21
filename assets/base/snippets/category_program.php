90367d9b1516e6e138b62c6bc34fe23a3aa9f638
Misc1170 <sloopy1170@gmail.com>
2023-07-21 11:56:31 +0300

    change migx-tv blocks under welcome text and img main page

--- assets/base/snippets/category_program.php
+++ assets/base/snippets/category_program.php
@@ -1,7 +1,8 @@
 <?php
 $query = $modx->query("SELECT * FROM `modx_site_tmplvars` WHERE name='category_program'");
 $result = $query->fetch(PDO::FETCH_ASSOC);
-$elements = explode('||',$result['elements']);
+$categories = explode('||', $result['elements']);
 
-// echo gettype($elements);
-// var_dump($elements);
+// foreach ($categories as $category) {
+//     echo $category .PHP_EOL;
+// }
